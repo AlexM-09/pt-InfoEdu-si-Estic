8f2f72aba906e0f04172d4c6f21a985583026c11
beres <beresclaudiualexandru@gmail.com>
2026-05-19 16:49:03 +0300

    Am reparat sunetul din Cutscene

--- Scenes/Maps/canvas_layer_menu.gd
+++ Scenes/Maps/canvas_layer_menu.gd
@@ -1,20 +1,18 @@
 extends CanvasLayer
+
 @onready var popup = $NamePopup
 @onready var name_input = $NamePopup/NameInput
 @onready var btn_confirm = $NamePopup/BtnConfirm
-@onready var btn_start = get_parent().get_node("VBoxContainer/start")
 
 func _ready():
 	popup.visible = false
 	btn_confirm.pressed.connect(_on_confirm_pressed)
-	# verifica daca nu e deja conectat inainte sa conecteze
 	if not get_parent().get_node("VBoxContainer/start").pressed.is_connected(_on_start_pressed):
 		get_parent().get_node("VBoxContainer/start").pressed.connect(_on_start_pressed)
 
 func _on_start_pressed():
 	popup.visible = true
 	name_input.text = SaveManager.data["character_name"]
-	
 	name_input.grab_focus()
 
 func _on_confirm_pressed():
@@ -24,4 +22,5 @@ func _on_confirm_pressed():
 		return
 	SaveManager.set_character_name(nume)
 	print("Nume setat: ", nume)
+	AudioManager.stop_music()
 	get_tree().change_scene_to_file("res://Scenes/CutscenePlayer.tscn")
